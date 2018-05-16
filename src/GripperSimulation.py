4088971b9296fa769277526daff803598fa1c35b
Unknown <omirrp28@hotmail.com>
2018-05-16 18:02:28 -0500

    Added rotation in y axis
    And minor text fixes

--- src/GripperSimulation.py
+++ src/GripperSimulation.py
@@ -11,11 +11,12 @@ mbrazo = 45.7
 
 
 class Barra:
-    def __init__(self, pbase, pfin, col=color.cyan):
+    def __init__(self, pbase, pfin, pnormal=vector(1, 0, 0), col=color.cyan):
         self.base = sphere(pos=pbase, radius=0.5, color=color.white)
         self.fin = sphere(pos=pfin, radius=0.5, color=color.white)
-        self.norma = sqrt((pbase.x - pfin.x) ** 2 + (pbase.y - pfin.y) ** 2 + (pbase.z - pfin.z) ** 2)
+#       self.norma = sqrt((pbase.x - pfin.x) ** 2 + (pbase.y - pfin.y) ** 2 + (pbase.z - pfin.z) ** 2)
         self.body = cylinder(pos=pbase, radius=0.5, axis=pfin - pbase, color=col)
+        self.motor = cylinder(pos=pbase, radius=0.1, axis=pnormal, color=color.white)
         self.atached = []
 
     def atach(self, barra):
@@ -24,34 +25,35 @@ class Barra:
     def rotate(self, angle):
         self.fin.rotate(angle, self.fin.pos - self.base.pos, self.base.pos)
         self.body.rotate(angle, self.fin.pos - self.base.pos, self.base.pos)
-        if (len(self.atached) > 0):
-            for x in self.atached:
-                x.rotateb(angle, self.fin.pos - self.base.pos, self.base.pos)
+        self.motor.rotate(angle, self.fin.pos - self.base.pos, self.base.pos)
+        if len(self.atached) > 0:
+            for elem in self.atached:
+                elem.__rotate(angle, self.fin.pos - self.base.pos, self.base.pos)
 
-    def rotatel(self, angle):
-        self.fin.rotate(angle, self.fin.pos - self.base.pos, self.base.pos)
-        self.body.rotate(angle, self.fin.pos - self.base.pos, self.base.pos)
-        if (len(self.atached) > 0):
-            for x in self.atached:
-                x.rotateb(angle, self.fin.pos - self.base.pos, self.base.pos)
+    def rotatem(self, angle):
+        self.fin.rotate(angle, self.motor.axis, self.base.pos)
+        self.body.rotate(angle, self.motor.axis, self.base.pos)
+        if len(self.atached) > 0:
+            for elem in self.atached:
+                elem.__rotate(angle, self.motor.axis, self.base.pos)
 
-    def rotateb(self, angle, axis, origin):
+    def __rotate(self, angle, axis, origin):
         self.fin.rotate(angle, axis, origin)
         self.base.rotate(angle, axis, origin)
         self.body.rotate(angle, axis, origin)
-        if (len(self.atached) > 0):
-            for x in self.atached:
-                x.rotateb(angle, axis, origin)
+        self.motor.rotate(angle, axis, origin)
+        if len(self.atached) > 0:
+            for elem in self.atached:
+                elem.__rotate(angle, axis, origin)
 
 
-x = Barra(vector(0, 0, 0), vector(0, mbase, 0))
-x2 = Barra(x.fin.pos, vector(0, mbase, mele1))
-x3 = Barra(x2.fin.pos, vector(0, mbase + mele2, mele1))
-xx1 = Barra(x3.fin.pos, vector(0, mbase + mele2, mele1 - mbrazo))
-x.atach(x2)
-x2.atach(x3)
-x3.atach(xx1)
+p = Barra(vector(0, 0, 0), vector(0, mbase, 0))
+p2 = Barra(p.fin.pos, vector(0, mbase, mele1), col=color.blue)
+p21 = Barra(p2.fin.pos, vector(0, mbase + mele2, mele1), col=color.blue)
+p3 = Barra(p21.fin.pos, vector(0, mbase + mele2, mele1 - mbrazo), col=color.yellow)
+p.atach(p2)
+p2.atach(p21)
+p21.atach(p3)
 
 while True:
-    x.rotate(0.001)
-    x2.rotate(0.001)
+    p.rotate(0.001)
